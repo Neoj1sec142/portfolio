671cf5ae9189417d12c4cbc75bf10efa52253a76
Mark Harmon <neothej1sec@gmail.com>
2022-09-10 16:31:49 -0500

    react icons setup

--- newport/portfolio/src/pages/Main.jsx
+++ newport/portfolio/src/pages/Main.jsx
@@ -4,15 +4,18 @@ import {RiGithubFill, RiLinkedinBoxFill, RiTwitterFill} from 'react-icons/ri'
 const Main = () => {
     return(
         <div>
-        <h1>Text</h1>
-        <div className="card main">
-            Main
-        </div>
-        <footer className='card main'>
-            <RiGithubFill className='icon' />
-            <RiLinkedinBoxFill className='icon'/>
-            <RiTwitterFill className='icon'/>
-        </footer>
+            <h1 className='main'>Mark Harmon Full Stack Software Developer</h1>
+            <div className="card main">
+                <div className='sidebar'>
+                    <button className='btn btn-outline-light'>Hello</button>
+                    <button className='btn btn-outline-light'>Hello</button>
+                </div>
+            </div>
+            <footer className='card main'>
+                <a href='https://www.github.com/neoj1sec142' target='_blank' rel="noreferrer"><RiGithubFill className='icon' /></a>
+                <a href='https://www.linkedin.com/in/markharmon142' target='_blank' rel="noreferrer"><RiLinkedinBoxFill className='icon'/></a>
+                <a href='https://www.twitter.com/ManicNeo142' target='_blank' rel="noreferrer"><RiTwitterFill className='icon'/></a>
+            </footer>
         </div>
     )
 }
