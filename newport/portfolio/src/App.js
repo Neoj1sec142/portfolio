ed0fba85d24164ad91d6809f3171996878eea1ab
Mark Harmon <neothej1sec@gmail.com>
2022-09-10 14:31:40 -0500

    starting new portfolio project

---
+++ newport/portfolio/src/App.js
@@ -0,0 +1,21 @@
+// Style Imports
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// Service Imports
+import { Route, Routes } from 'react-router-dom';
+// Page / Component Imports
+import Main from './pages/Main';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
