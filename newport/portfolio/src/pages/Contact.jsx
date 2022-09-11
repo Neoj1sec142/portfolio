e65e1233387316a1c987dc3b51e0c4f616932feb
Mark Harmon <neothej1sec@gmail.com>
2022-09-11 16:11:41 -0500

    working on about modal

--- newport/portfolio/src/pages/Contact.jsx
+++ newport/portfolio/src/pages/Contact.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/contactcard.css'
 import pro from '../assets/profile.png'
+import  Modal  from '../components/Modal'
+
+
 const Contact = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return(
         <div>
             <br></br>
@@ -12,12 +16,15 @@ const Contact = () => {
                 <p>Full Stack Software Developer</p>
                 <p>Interested in Web technologies like HTML5, CSS3, JavaScript, Python, SQL, etc.</p>
                 <p>Love PicoCTF and respect Guido van Rossum. ;)</p>
-                <span class="left bottom">mharmondev1503@gmail.com</span>
-                <span class="right bottom">@ManicNeo142</span>
+                <span className="left bottom">mharmondev1503@gmail.com</span>
+                <span className="right bottom">@ManicNeo142</span>
             </div>
             <br></br>
             <div id='about'>
-                About Me:
+            <button className="primaryButton" onClick={() => setIsOpen(true)}>
+                About Me
+            </button>
+                {isOpen && <Modal setIsOpen={setIsOpen} />}
             </div>
         </div>
     )
